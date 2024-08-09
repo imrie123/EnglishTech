d146055944fd2c62384d79915e1853b74c77972c
imrie123 <nosirie@ymobile.ne.jp>
2024-08-09 09:55:20 +0900

    Add Firebase authentication functionality

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  before_action :load_dotenv
+
+  def verify_firebase_token(token)
+
+    public_keys = FirebaseService::fetch_firebase_public_keys
+    jwt_header = JWT.decode(token, nil, false)[1]
+    kid = jwt_header['kid']
+    public_key = OpenSSL::X509::Certificate.new(public_keys[kid]).public_key
+
+    JWT.decode(token, public_key, true, { algorithm: 'RS256' })
+  rescue => e
+    Rails.logger.error "JWT Verification Error: #{e.message}"
+    render json: { error: 'Invalid token' }, status: :unauthorized
+  end
+
+  private
+
+  def load_dotenv
+    Dotenv::Rails.load
+  end
 end
---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def create
+
+    @response = FirebaseService::SignUp.new(user_params[:email], user_params[:password]).call
+    if @response["idToken"]
+      @user = User.new(user_params)
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    else
+      render json: { error: 'Failed to create user in Firebase' }, status: :unprocessable_entity
+    end
+  end
+
+  def sign_in
+    @response = FirebaseService::SignIn.new(user_params[:email], user_params[:password]).call
+    if @response["idToken"]
+      @user = User.find_by(email: user_params[:email])
+      render "sign_in", formats: :json, handlers: :jbuilder, status: :ok
+    else
+      @error = @response["error"]["message"]
+      render "error", formats: :json, handlers: :jbuilder, status: :unauthorized
+    end
+  end
+
+  def sign_out
+    FirebaseService::SignOut.new(session[:token]).call
+    head :ok
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
